3988b30bb51c51e859279a3d6586be94b7a4994a
GabOof <oliveirafonseca.gab@gmail.com>
2025-01-28 23:10:49 -0300

    Adiciona função para contar componentes conexos em um grafo

--- src/script.js
+++ src/script.js
@@ -39,3 +39,35 @@ function criarGrafo(arestas) {
 
   return grafo;
 }
+
+// Função para contar o número de componentes conexos no grafo
+function contarComponentesConexos(grafo) {
+  const visitado = new Set(); // Cria um conjunto para armazenar os nodos visitados
+
+  let componentesConexos = 0; // Inicializa o contador de componentes conexos
+
+  for (const nodo in grafo) {
+    // Verifica se o nodo já foi visitado e, se não, realiza busca por profundidade
+    if (!visitado.has(Number(nodo))) {
+      buscaProfundidade(grafo, Number(nodo), visitado); // Realiza busca por profundidade para marcar todos os nodos conectados
+      componentesConexos++; // Incrementa o contador de componentes conexos
+    }
+  }
+  return componentesConexos;
+}
+
+// Função auxiliar para realizar busca por profundidade
+function buscaProfundidade(grafo, nodo, visitado) {
+  if (!grafo[nodo]) {
+    return; // Se o nó não estiver no grafo, retorna
+  }
+
+  visitado.add(nodo); // Marca o nó como visitado
+
+  grafo[nodo].forEach((v) => {
+    // Verifica se o vizinho não foi visitado e, se não, realiza busca por profundidade
+    if (!visitado.has(v)) {
+      buscaProfundidade(grafo, v, visitado); // Realiza busca por profundidade para marcar todos os nodos conectados
+    }
+  });
+}
